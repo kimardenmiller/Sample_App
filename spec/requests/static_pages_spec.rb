8c6ba4f61a80c4695a522bf3a04ae9cc90c87e1e
Kim Miller <kimardenmiller@gmail.com>
2012-10-31 10:27:35 -0700

    After env recovery

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,9 @@
-require 'spec_helper'
+require 'rspec'
 
-describe "Static pages" do
-
+describe "StaticPages" do
   describe "Home page" do
-
     it "should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
