3bc68867cb834acfe5a2c0b15cc00ddf5bacdbe8
Kim Miller <kimardenmiller@gmail.com>
2012-11-11 15:19:01 -0800

    Modeling Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
+# app/models/user.rb
+class User < ParseUser
+  # you can add fields, like any other kind of Object...
+  fields :name, :bio
+
+  # but note that email is a special field in the Parse API.
+  fields :email
+
+  # no validations included, but feel free to add your own
+  #validates_presence_of :username
+
+
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+end
+
+
 #class User < ParseResource::Base
 #  field :name
 #  field :email
@@ -5,10 +26,10 @@
 #end
 
 
-class User < ActiveRecord::Base
-  attr_accessible :email, :name
-
-  validates :name, presence: true
-  #validates :email, UniquenessValidator: true
-
-end
+#class User < ActiveRecord::Base
+#  attr_accessible :email, :name
+#
+#  validates :name, presence: true
+#  #validates :email, UniquenessValidator: true
+#
+#end
