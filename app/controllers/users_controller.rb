53410642ae95769ea2c21c0246ad0edce0a0bc4d
Kim Miller <kimardenmiller@gmail.com>
2012-11-15 13:01:31 -0800

    Error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to @user, :notice => "You are now Signed up!"
+      #redirect_to @user, :notice => "You are now Signed up! (Older)"
+      flash[:success] = "Welcome to the Sample App!"
       #redirect_to root_url, :notice => "Signed up!"
     else
       render "new"
